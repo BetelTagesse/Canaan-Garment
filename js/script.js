42fa2fd5beeccc7666e8f5f3aa5f8e8cf2ad62b0
Betel Tagesse <BetelTagesse367@gmail.com>
2023-02-22 08:02:24 +0300

    Project added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let hamburger = document.getElementById('nav-hamburger');
+let navList = document.getElementById('nav-list');
+
+hamburger.addEventListener('click', () => {
+    navList.classList.toggle('nav__list--show');
+});
+
+
